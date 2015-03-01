e3dc05aff15bb93fef8871bdd9e9feaefccba06e
cnichols <mellowdious@mail.com>
2015-03-01 07:01:16 -0800

    mvp new trip email notification

--- server/routes/trips/create.js
+++ server/routes/trips/create.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var Trip = require('../../models/trip');
+var Message = require('../../models/message');
+var User = require('../../models/user');
 
 module.exports = {
   handler: function(request, reply) {
@@ -8,7 +10,14 @@ module.exports = {
     request.payload.userId = request.auth.credentials._id;
     var trip = new Trip(request.payload);
     trip.save(function(){
-      reply({url:'/trips/' + trip._id + '/itinerary'});
+      User.findById(request.payload.userId, function(err, user) {
+        Message.trip(user.email, trip, function(e, result) {
+          console.log('*******USEREMAIL: ', user.email);
+          console.log('*******TRIP: ', trip);
+          console.log('*******RESULT: ', result);
+          reply({url:'/trips/' + trip._id + '/itinerary'});
+        });
+      });
     });
   }
 };
