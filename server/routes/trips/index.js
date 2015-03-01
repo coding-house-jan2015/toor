46be696306552d91f29b7539534af3f039e85e00
cnichols <mellowdious@mail.com>
2015-03-01 07:55:38 -0800

    wip trips/index

--- server/routes/trips/index.js
+++ server/routes/trips/index.js
@@ -1,10 +1,16 @@
 'use strict';
 
 var Trip = require('../../models/trip');
+var moment = require('moment');
 
 module.exports = {
   handler: function(request, reply) {
     Trip.find({userId:request.auth.credentials._id}, function(err, trips){
+      console.log(trips[0]);
+      trips.map(function(trip) {
+        trip.departureDate = moment(trip.departureDate).format('LL');
+        trip.returnDate = moment(trip.returnDate).format('LL');
+      });
       reply.view('templates/trips/index', {trips:trips});
     });
   }
