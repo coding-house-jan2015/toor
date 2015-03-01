330421344e307f43bed920222861acb80f95cd17
Chyld Medford <chyld.medford@gmail.com>
2015-03-01 10:00:45 -0800

    prefill date

--- server/routes/trips/new.js
+++ server/routes/trips/new.js
@@ -1,7 +1,9 @@
 'use strict';
 
+var moment = require('moment');
+
 module.exports = {
   handler: function(request, reply) {
-    reply.view('templates/trips/new');
+    reply.view('templates/trips/new', {moment:moment});
   }
 };
