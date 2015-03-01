358f903811100b3d80a4a17a07c180c886ba4fe8
Spinaldash <blueavenger@gmail.com>
2015-03-01 03:20:57 -0800

    god bless this merge

--- client/js/passValidation.js
+++ client/js/passValidation.js
@@ -10,14 +10,12 @@ var confirm_password = document.getElementById("confirmPass");
 
 function validatePassword(){
   if(password.value != confirm_password.value) {
-    console.log('SHIT IS NOT RIGHT');
     $('#inputError').removeClass('form-group has-success has-feedback');
     $('#inputError').addClass('form-group has-error has-feedback');
     $('#checkIcon').css({"visibility": "hidden"});
     $('#xIcon').css({"visibility": "visible"});
     $('#register').addClass('disabled');
   } else {
-    console.log('SHIT IS RIGHT');
     $('#inputError').removeClass('form-group has-error has-feedback');
     $('#inputError').addClass('form-group has-success has-feedback');
     $('#xIcon').css({"visibility": "hidden"});
