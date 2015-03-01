90ea88c229341e20d37775ad76c6eb32df93a0ae
cnichols <mellowdious@mail.com>
2015-03-01 11:02:32 -0800

    mvp my trips page

--- server/routes/trips/index.js
+++ server/routes/trips/index.js
@@ -6,12 +6,7 @@ var moment = require('moment');
 module.exports = {
   handler: function(request, reply) {
     Trip.find({userId:request.auth.credentials._id}, function(err, trips){
-      console.log(trips[0]);
-      trips.map(function(trip) {
-        trip.departureDate = moment(trip.departureDate).format('LL');
-        trip.returnDate = moment(trip.returnDate).format('LL');
-      });
-      reply.view('templates/trips/index', {trips:trips});
+      reply.view('templates/trips/index', {trips:trips, moment:moment});
     });
   }
 };
